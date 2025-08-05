1577a6cd3fcd39e1bb6eb5e32f33edd5350dafb6
Ace1116 <bigeshmohzn@gmail.com>
2025-08-05 07:58:24 +0545

    Second code push

--- fruits.py
+++ fruits.py
@@ -1,22 +1,6 @@
-def sq(x):
-    return x**2
-
-result = sq(5)
-print("result",result)
-
-
-
-lst=[1, 2, 3, 4, 5]
-squared =list(map(lambda x: x**2, lst))
-print("squared", squared)
-
-numbers=[1, 2, 3, 4, 5,6,7,8,9,10]
-even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
-print("even_numbers", even_numbers)`
+print("This is the fruits module.")
 
 
 
 
- import os
-def get_fruit_list():
-    return ["apple", "banana", "cherry"]
+
